92bc3496957b6e492d85532f1fab4355c5552916
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2020-11-24 17:19:21 +0100

    added buttons and redirections

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_11_24_142130) do
-
+ActiveRecord::Schema.define(version: 2020_11_24_143810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,4 +97,3 @@ ActiveRecord::Schema.define(version: 2020_11_24_142130) do
   add_foreign_key "influencers", "users"
   add_foreign_key "reviews", "bookings"
 end
- 
