105078773b346ffc91b0f7e73c5019eef0e2b3ca
julien catonnet <julien.catonnet@essec.edu>
2020-11-24 14:48:27 +0100

    changed routes & made campagin controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,3 +1,39 @@
 class CampaignsController < ApplicationController
 
+  def new
+    @company = Company.find(params[:company_id])
+    @campaign = Campaign.new
+    authorize @campaign
+  end
+  
+  def create
+    @company = Company.find(params[:company_id])
+    @campaign = Campaign.new(campaign_params)
+    @campaign.user = current_user
+    @campaign.company = @company
+    authorize @campaign
+    if @campaign.save!
+        redirect_to campaign_path
+    else
+        render :new
+    end
+  end
+
+  def edit
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:company_id])
+    @campaign = Campaign.find(params[:id])
+    authorize @compaign
+    @campaign.update(campaign_params)
+    redirect_to campaign_path(@campaign)
+  end
+
+private
+
+def campaign_params
+    params.require(:campaign).permit(:name, :description, :target, :occurs_at, :status, :number_of_stories, :number_of_photos, :number_of_videos, :number_of_lives)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :companies, only: [:show, :new, :create, :update, :edit]
+  resources :companies, only: [:show, :new, :create, :update, :edit] do
+    resources :campaigns, only: [:new, :create]
+  end
+  
+  resources :campaigns, only: [:show, :edit, :update]
 
   resources :influencers, only: [:new, :create, :update, :edit, :show] do
-    resources :bookings, only: [:edit, :update]
+    resources :campaigns, only: [:destroy] do
+      resources :bookings, only: [:new, :create]
+    end
   end
 
-  resources :campaigns do
-      resources :bookings, only: [:new, :create, :index]
-  end
-
-  resources :bookings, only: [:destroy] do
+  resources :bookings, only: [:edit, :update, :destroy] do 
     resources :reviews, only: [:new, :create]
   end
 
