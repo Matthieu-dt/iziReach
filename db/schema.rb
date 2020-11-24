4b0ca8c8f326555adba4a0f2d3b665b87f54ca45
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2020-11-24 15:51:16 +0100

    modifications on  booking model and booking controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_112021) do
+ActiveRecord::Schema.define(version: 2020_11_24_142130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_112021) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "influencer_id", null: false
     t.bigint "campaign_id", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.text "contract"
     t.integer "total_price"
     t.datetime "created_at", precision: 6, null: false
