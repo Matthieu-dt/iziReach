d97a44b9ba5c1e7692abefcf7fb95ff62283ddf6
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2020-11-25 11:20:18 +0100

    validations on review model, new view and strong params in the controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,4 +16,10 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
