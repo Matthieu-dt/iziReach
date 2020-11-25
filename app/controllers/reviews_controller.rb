cf90d41a6c8d6398836b4b07200fd20f01fbd142
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2020-11-25 10:58:27 +0100

    reviews controllers, methods done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(booking: @booking)
+    authorize @review
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    authorize @review
+    if @review.save
+      redirect_to campaign_path(@review.booking.campaign)
+    else
+      render :new
+    end
+  end
 end
